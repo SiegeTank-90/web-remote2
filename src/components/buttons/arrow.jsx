fb19395052c9065ad1db5d86bed65d463132e107
SiegeTank-90 <ccline140@yahoo.com>
2022-06-24 13:22:59 -0500

    Movile Button Testing.

--- src/components/buttons/arrow.jsx
+++ src/components/buttons/arrow.jsx
@@ -8,22 +8,21 @@ function Arrow({ rotate }) {
     const [size, setSize] = useState('2x')
     const [classname, setClassName] = useState('')
 
-    function caretOn(e) {
-
-        setActiveState(faCaretUp)
-        setSize('6x')
-        setClassName('highlight')
+    function caret(e) {
+        if (e.target.type === 'touchstart') {
+            e.preventDefault()
+            setActiveState(faCaretUp)
+            setSize('6x')
+            setClassName('highlight')
+        } else {
+            setActiveState(faChevronUp)
+            setSize('2x')
+            setClassName('')
+        }
     }
-    function caretOff() {
-        // setActiveState(faChevronUp)
-        // setSize('2x')
-        // setClassName('')
-
-    }
-
 
     return (
-        <FontAwesomeIcon onMouseDown={caretOn} onMouseUp={caretOff} icon={activeState} className={classname} transform={{ rotate: rotate }} size={size} />
+        <FontAwesomeIcon ontTouchStart={caret} onTouchEnd={caret} icon={activeState} className={classname} transform={{ rotate: rotate }} size={size} />
     )
 }
 export default Arrow
--- src/components/buttons/navpad.jsx
+++ src/components/buttons/navpad.jsx
@@ -7,38 +7,36 @@ function NavPad() {
 
 
 
-    function HandleMouseDown(e) {
+    function handleTouch(e) {
         e.preventDefault();
-        console.log('down')
-        if (e.target.closest('button').className === "NavPad--Direction--Up") {
-            setDirection('highlight-up')
+        if (e.target.type === 'touchstart') {
+            if (e.target.closest('button').className === "NavPad--Direction--Up") {
+                setDirection('highlight-up')
+            }
+            if (e.target.closest('button').className === "NavPad--Direction--Left") {
+                setDirection('highlight-left')
+            }
+            if (e.target.closest('button').className === "NavPad--Direction--Right") {
+                setDirection('highlight-right')
+            }
+            if (e.target.closest('button').className === "NavPad--Direction--Down") {
+                setDirection('highlight-down')
+            }
+        } else {
+            setDirection('')
         }
-        if (e.target.closest('button').className === "NavPad--Direction--Left") {
-            setDirection('highlight-left')
-        }
-        if (e.target.closest('button').className === "NavPad--Direction--Right") {
-            setDirection('highlight-right')
-        }
-        if (e.target.closest('button').className === "NavPad--Direction--Down") {
-            setDirection('highlight-down')
-        }
-    }
 
-    function HandleMouseUp() {
-        // console.log('up')
-        // setDirection('')
     }
-
 
     return (
         <div className="NavPad">
             <div className={`NavPad--Direction ${Direction}`}>
                 <div className="NavPad--Direction--Grid">
-                    <button onMouseDown={HandleMouseDown} onMouseUp={HandleMouseUp} className="NavPad--Direction--Enter" > <div className="NavPad--Direction--Enter-Icon"></div></button>
-                    <button onMouseDown={HandleMouseDown} onMouseUp={HandleMouseUp} className="NavPad--Direction--Up"> <Arrow rotate={0} /></button>
-                    <button onMouseDown={HandleMouseDown} onMouseUp={HandleMouseUp} className="NavPad--Direction--Left"> <Arrow rotate={270} /></button>
-                    <button onMouseDown={HandleMouseDown} onMouseUp={HandleMouseUp} className="NavPad--Direction--Right"> <Arrow rotate={90} /></button>
-                    <button onMouseDown={HandleMouseDown} onMouseUp={HandleMouseUp} className="NavPad--Direction--Down"> <Arrow rotate={180} /></button>
+                    <button onTouchStart={handleTouch} onTouchEnd={handleTouch} className="NavPad--Direction--Enter" > <div className="NavPad--Direction--Enter-Icon"></div></button>
+                    <button onTouchStart={handleTouch} onTouchEnd={handleTouch} className="NavPad--Direction--Up"> <Arrow rotate={0} /></button>
+                    <button onTouchStart={handleTouch} onTouchEnd={handleTouch} className="NavPad--Direction--Left"> <Arrow rotate={270} /></button>
+                    <button onTouchStart={handleTouch} onTouchEnd={handleTouch} className="NavPad--Direction--Right"> <Arrow rotate={90} /></button>
+                    <button onTouchStart={handleTouch} onTouchEnd={handleTouch} className="NavPad--Direction--Down"> <Arrow rotate={180} /></button>
 
                 </div>
             </div>
