13b6a86d93730ec8aa7405a355447448ca748e0e
SiegeTank-90 <ccline140@yahoo.com>
2022-07-11 10:00:47 -0500

    another attempt to fix broken links

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,28 +1,53 @@
-import React from "react";
-import MenuNavigationButton from "./buttons/menuNavigationButton";
-import appIcon from '../images/app.svg'
-import deviceIcon from '../images/devices.svg'
-import remoteIcon from '../images/remote.svg'
-import liveTVIcon from '../images/LiveTv.svg'
-import appIconActive from '../images/app-active.svg'
-import remoteIconActive from '../images/remote-active.svg'
-import liveTVIconActive from '../images/liveTv-active.svg'
-import deviceIconActive from '../images/devices-active.svg'
+import React from 'react';
+import MenuNavigationButton from './buttons/menuNavigationButton';
+import appIcon from '../images/app.svg';
+import deviceIcon from '../images/devices.svg';
+import remoteIcon from '../images/remote.svg';
+import liveTVIcon from '../images/liveTv.svg';
+import appIconActive from '../images/app-active.svg';
+import remoteIconActive from '../images/remote-active.svg';
+import liveTVIconActive from '../images/liveTv-active.svg';
+import deviceIconActive from '../images/devices-active.svg';
 
 function Menu({ menuIndex, setMenuIndex }) {
-
-
-
     return (
-        <div className="Menu">
-            <div className="Menu--Navigation">
-                <MenuNavigationButton iconActive={remoteIconActive} icon={remoteIcon} name='Remote' index={1} menuIndex={menuIndex} setMenuIndex={setMenuIndex} />
-                <MenuNavigationButton iconActive={appIconActive} icon={appIcon} name='Apps' index={2} menuIndex={menuIndex} setMenuIndex={setMenuIndex} />
-                <MenuNavigationButton iconActive={liveTVIconActive} icon={liveTVIcon} name='Live TV' index={3} menuIndex={menuIndex} setMenuIndex={setMenuIndex} />
-                <MenuNavigationButton iconActive={deviceIconActive} icon={deviceIcon} name='Devices' index={4} menuIndex={menuIndex} setMenuIndex={setMenuIndex} />
+        <div className='Menu'>
+            <div className='Menu--Navigation'>
+                <MenuNavigationButton
+                    iconActive={remoteIconActive}
+                    icon={remoteIcon}
+                    name='Remote'
+                    index={1}
+                    menuIndex={menuIndex}
+                    setMenuIndex={setMenuIndex}
+                />
+                <MenuNavigationButton
+                    iconActive={appIconActive}
+                    icon={appIcon}
+                    name='Apps'
+                    index={2}
+                    menuIndex={menuIndex}
+                    setMenuIndex={setMenuIndex}
+                />
+                <MenuNavigationButton
+                    iconActive={liveTVIconActive}
+                    icon={liveTVIcon}
+                    name='Live TV'
+                    index={3}
+                    menuIndex={menuIndex}
+                    setMenuIndex={setMenuIndex}
+                />
+                <MenuNavigationButton
+                    iconActive={deviceIconActive}
+                    icon={deviceIcon}
+                    name='Devices'
+                    index={4}
+                    menuIndex={menuIndex}
+                    setMenuIndex={setMenuIndex}
+                />
             </div>
         </div>
-    )
+    );
 }
 
-export default Menu
+export default Menu;
